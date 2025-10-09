67e2eff5b1e9686e86ba2c5de91405818e45775f
Sebasln <sebastian.silva1.dosa@gmail.com>
2025-10-09 14:19:16 +0200

    clase 09/10/25

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addToDo = document.getElementById('addToDo');
+    const addDoing = document.getElementById('addDoing');
+    const addDone = document.getElementById('addDone');
+    
+    addToDo.addEventListener('click', function() {
+
+        console.log("hola")
+    });
+
+});
